2556d6e23acab9978a81b2032cde1a7f5325ae6a
Luis de la Garza <delagarza@gmail.com>
2019-02-13 14:43:09 +0100

    openBIS connection added; cleaned up unused code; now using DB as given in properties config file

--- src/main/java/life/qbic/components/qOfferManager.java
+++ src/main/java/life/qbic/components/qOfferManager.java
@@ -22,6 +22,7 @@ import com.vaadin.ui.GridLayout.OverlapsException;
 import com.vaadin.ui.themes.ValoTheme;
 import life.qbic.dbase.DBManager;
 import life.qbic.dbase.Database;
+import life.qbic.dbase.OpenBisProxy;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 

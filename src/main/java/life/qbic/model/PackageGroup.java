eac2a66b3644cb5f06e37bf250bb1d67709d7f84
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2019-05-15 18:06:28 +0200

    add enum for package groups

---
+++ src/main/java/life/qbic/model/PackageGroup.java
@@ -0,0 +1,15 @@
+package life.qbic.model;
+
+public enum PackageGroup {
+
+  Sequencing, Mass_Spectrometry, Bioinformatics_Analysis, Project_Management;
+
+  public static boolean contains(String test) {
+    for (PackageGroup c : PackageGroup.values()) {
+      if (c.name().equals(test)) {
+        return true;
+      }
+    }
+    return false;
+  }
+}

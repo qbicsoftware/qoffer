e4115a0960eca36be795ebb9d49a549835b080ac
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2019-08-06 16:57:41 +0200

    add group 'Other'

--- src/main/java/life/qbic/model/PackageGroup.java
+++ src/main/java/life/qbic/model/PackageGroup.java
@@ -2,7 +2,7 @@ package life.qbic.model;
 
 public enum PackageGroup {
 
-  Sequencing, Mass_Spectrometry, Bioinformatics_Analysis, Project_Management;
+  Sequencing, Mass_Spectrometry, Bioinformatics_Analysis, Project_Management, Other;
 
   public static boolean contains(String test) {
     for (PackageGroup c : PackageGroup.values()) {

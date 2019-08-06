6d2aef6743808b5042376eb99c4d42bf2ea9ef05
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2019-08-06 17:01:22 +0200

    better currency formatting

--- src/main/java/life/qbic/utils/qOfferManagerUtils.java
+++ src/main/java/life/qbic/utils/qOfferManagerUtils.java
@@ -21,7 +21,6 @@ import com.vaadin.server.FileDownloader;
 import com.vaadin.server.FileResource;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.server.Page;
-import com.vaadin.server.VaadinService;
 import com.vaadin.shared.Position;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.themes.ValoTheme;
@@ -29,57 +28,81 @@ import com.vaadin.ui.themes.ValoTheme;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public final class qOfferManagerUtils {
 
-  /**
-   * Both Database and OpenBisProxy require this path to obtain configuration settings.
-   */
-  public static final String PROPERTIES_FILE_PATH = VaadinService.getCurrent().getBaseDirectory().getAbsolutePath() + "/WEB-INF/resourceFiles/Credentials.properties";
+//  /**
+//   * Both Database and OpenBisProxy require this path to obtain configuration settings.
+//   */
+//  public static final String PROPERTIES_FILE_PATH =
+//      VaadinService.getCurrent().getBaseDirectory().getAbsolutePath()
+//          + "/WEB-INF/resourceFiles/Credentials.properties";
+
+  private static final Logger LOG = LogManager.getLogger(qOfferManagerUtils.class);
 
   /**
    * Formats a given string to X,XXX.XX €.
+   * 
    * @param currencyToFormat: string we want to format
    * @return currency in X,XXX.XX € format
    */
   public static String formatCurrency(String currencyToFormat) {
+    try {
+      NumberFormat format = NumberFormat.getCurrencyInstance(Locale.UK);
+      format.setCurrency(Currency.getInstance("EUR"));
+
+      currencyToFormat = format.format(new BigDecimal(currencyToFormat));
+    } catch (Exception e) {
+      LOG.warn("could not format currency string for "+currencyToFormat);
+      LOG.warn(e.getLocalizedMessage());
+    }
+    return currencyToFormat;
 
     // TODO: check if this works all the time
     // format should be something like X.XXX,XX
-    if (currencyToFormat.contains(",")) {
-      String[] temp = currencyToFormat.split(",");
-      // format is weird, so we return it without changing it..
-      if (temp.length > 2) {
-        return currencyToFormat;
-      }
-      // price is e.g. X.XXX,X, so we add a single 0 at the end to it
-      if (temp[temp.length-1].length() == 1) {
-        currencyToFormat += "0";
-      }
-
-      // replace commas with dots and vice versa
-      currencyToFormat = currencyToFormat.replace(".", "@");
-      currencyToFormat = currencyToFormat.replace(",", "?");
-      currencyToFormat = currencyToFormat.replace("@", ",");
-      currencyToFormat = currencyToFormat.replace("?", ".");
-
-      return currencyToFormat + " €";
-    } else
-      // format is X.XXX, so we change it to X,XXX.00 €
-      return currencyToFormat.replace(".", ",") + ".00 €";
+    // if (currencyToFormat.contains(",")) {
+    // String[] temp = currencyToFormat.split(",");
+    // // format is weird, so we return it without changing it..
+    // if (temp.length > 2) {
+    // return currencyToFormat;
+    // }
+    // // price is e.g. X.XXX,X, so we add a single 0 at the end to it
+    // if (temp[temp.length-1].length() == 1) {
+    // currencyToFormat += "0";
+    // }
+    //
+    // // replace commas with dots and vice versa
+    // currencyToFormat = currencyToFormat.replace(".", "@");
+    // currencyToFormat = currencyToFormat.replace(",", "?");
+    // currencyToFormat = currencyToFormat.replace("@", ",");
+    // currencyToFormat = currencyToFormat.replace("?", ".");
+    //
+    // return currencyToFormat + " €";
+    // } else
+    // // format is X.XXX, so we change it to X,XXX.00 €
+    // return currencyToFormat.replace(".", ",") + ".00 €";
   }
 
   /**
    * Displays a vaadin Notification with the respective title, description and type.
+   * 
    * @param title: title of the displayNotification window
    * @param description: description of the displayNotification Window
-   * @param type: one of "error", "success" and "warning". Changes the style and the delay of the displayNotification.
+   * @param type: one of "error", "success" and "warning". Changes the style and the delay of the
+   *        displayNotification.
    */
   public static void displayNotification(String title, String description, String type) {
     com.vaadin.ui.Notification notify = new com.vaadin.ui.Notification(title, description);
     notify.setPosition(Position.TOP_CENTER);
     switch (type) {
-      case "error": //16000
+      case "error": // 16000
         notify.setDelayMsec(16000);
         notify.setIcon(FontAwesome.FROWN_O);
         notify.setStyleName(ValoTheme.NOTIFICATION_ERROR + " " + ValoTheme.NOTIFICATION_CLOSABLE);
@@ -100,15 +123,17 @@ public final class qOfferManagerUtils {
         notify.setStyleName(ValoTheme.NOTIFICATION_TRAY + " " + ValoTheme.NOTIFICATION_CLOSABLE);
         break;
     }
-    // make sure that, if we are running this from a background thread, we are accessing the UI in a thread-safe way
+    // make sure that, if we are running this from a background thread, we are accessing the UI in a
+    // thread-safe way
     UI.getCurrent().access(() -> {
-        notify.show(Page.getCurrent());
+      notify.show(Page.getCurrent());
     });
 
   }
 
   /**
    * returns the contents of the SQLContainer (including the header) as delimiter separated string
+   * 
    * @param container: SQLContainer to get the data from
    * @param delimiter: how the items should be separated, e.g. "," for .csv or "\t" for .tsv
    * @return delimiter separated string
@@ -118,25 +143,30 @@ public final class qOfferManagerUtils {
     StringBuilder containerContents = new StringBuilder();
 
     // write the header row
-    for (Object columnId: container.getContainerPropertyIds()) {
+    for (Object columnId : container.getContainerPropertyIds()) {
       containerContents.append(columnId.toString()).append(delimiter);
     }
     // remove last delimiter
-    containerContents = new StringBuilder(containerContents.substring(0, containerContents.length() - delimiter.length()));
+    containerContents = new StringBuilder(
+        containerContents.substring(0, containerContents.length() - delimiter.length()));
     containerContents.append("\n");
 
     // iterate over the row ids
-    for (Object rowId: container.getItemIds()) {
+    for (Object rowId : container.getItemIds()) {
       // iterate over the column ids
-      for (Object columnId: container.getContainerPropertyIds()) {
+      for (Object columnId : container.getContainerPropertyIds()) {
         try {
-          containerContents.append(container.getContainerProperty(rowId, columnId.toString()).getValue().toString()).append(delimiter);
+          containerContents
+              .append(
+                  container.getContainerProperty(rowId, columnId.toString()).getValue().toString())
+              .append(delimiter);
         } catch (NullPointerException e) {
           containerContents.append("null").append(delimiter);
         }
       }
       // remove last delimiter
-      containerContents = new StringBuilder(containerContents.substring(0, containerContents.length() - delimiter.length()));
+      containerContents = new StringBuilder(
+          containerContents.substring(0, containerContents.length() - delimiter.length()));
       containerContents.append("\n");
     }
 
@@ -145,12 +175,13 @@ public final class qOfferManagerUtils {
 
   /**
    * creates the content for exporting the tables as .csv file
+   * 
    * @param container: SQLContainer which should be exported as .csv
    * @param exportFileName: filename of the .csv file
    * @param fileDownloader: file downloader for downloading the .csv file
    */
-  public static void createExportContent(SQLContainer container, String exportFileName, FileDownloader fileDownloader)
-  {
+  public static void createExportContent(SQLContainer container, String exportFileName,
+      FileDownloader fileDownloader) {
     try {
       // get the contents of the container as a comma separated string
       String containerContents = getContainerContents(container, ",");
